f9ee8c717319322b5c8f6fc4d76eb8a0d9db542c
HLLLG <2162803956@qq.com>
2026-05-25 19:46:00 +0800

    fix

--- src/main/java/com/hl/hlojcodesandbox/controller/CodeSandboxController.java
+++ src/main/java/com/hl/hlojcodesandbox/controller/CodeSandboxController.java
@@ -3,6 +3,8 @@ package com.hl.hlojcodesandbox.controller;
 import com.hl.hlojcodesandbox.CodeSandbox;
 import com.hl.hlojcodesandbox.model.CodeSandboxRequest;
 import com.hl.hlojcodesandbox.model.CodeSandboxResult;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,11 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/executeCode")
 public class CodeSandboxController {
 
+    private static final String AUTH_REQUEST_HEADER = "Authorization";
+
+    private static final String AUTH_REQUEST_SECRET = "secretKey";
+
     @Autowired
     private CodeSandbox codeSandbox;
 
     @PostMapping
-    public CodeSandboxResult executeCode(@RequestBody CodeSandboxRequest request) {
+    public CodeSandboxResult executeCode(@RequestBody CodeSandboxRequest request, HttpServletRequest httpServletRequest,
+                                         HttpServletResponse response) {
+        String header = httpServletRequest.getHeader(AUTH_REQUEST_HEADER);
+        // 1. 鉴权
+        if (!AUTH_REQUEST_SECRET.equals(header)) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return null;
+        }
         return codeSandbox.executeCode(request);
     }
 }
